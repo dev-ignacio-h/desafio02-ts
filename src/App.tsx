e2b5c12992859a152b09331a9e096ba31ca9d34c
Ignacio <55958716+dev-ignacio-h@users.noreply.github.com>
2023-02-10 17:29:48 -0300

    chore: desafio 2 react-ts pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { 
-  Center,
-  ChakraProvider,
-  Input,
-  Box,
-  Button
-} from '@chakra-ui/react'
+import { Center, ChakraProvider, Input, Box, Button } from '@chakra-ui/react';
 import { login } from './services/login';
+import { Header } from './components/Header/Header';
+import DButton from './components/Header/DButton';
+import { Card } from './components/Card';
 
 function App() {
   return (
     <ChakraProvider>
-      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
-        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
+      <Header />
+      <Box minHeight="100vh" backgroundColor="#afffff" padding="25px">
+        <Card>
           <Center>
-            <h1>Faça o login</h1>
+            <h1>Login</h1>
           </Center>
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Center>
-            <Button onClick={login} colorScheme='teal' size='sm' width='100%' marginTop='5px'>
-              Button
-            </Button>
+          <Input placeholder="email" marginTop={3} />
+          <Input placeholder="password" marginTop={2} />
+          <Center marginTop={2}>
+            <DButton onClick={login} />
           </Center>
-        </Box>
+        </Card>
       </Box>
     </ChakraProvider>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
-import './Header.css'
+import { Box, Button, Center, Flex, Spacer, Text } from '@chakra-ui/react';
 
-export const Header  = () => {
-  return(
-    <div className='header'>
-      Dio Bank
-    </div>
-  )
-}
+export const Header = () => {
+  return (
+    <Flex backgroundColor="#2C7A7B" padding="10px">
+      <Box>
+        <Center>
+          <Text color="#afffff" fontSize="3xl">
+            Dio Bank
+          </Text>
+        </Center>
+      </Box>
+      <>
+        <Spacer />
+        <Button backgroundColor="#afffff" color="#2C7A7B">
+          Logout
+        </Button>
+      </>
+    </Flex>
+  );
+};
